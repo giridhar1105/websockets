5b93374c833c5ad54417f31849c6086ffb49a504
Giridhar <giridhaar1105@gmail.com>
2025-02-09 10:40:50 +0530

    done

--- dist/index.js
+++ dist/index.js
@@ -49,4 +49,19 @@ wss.on('connection', function connection(ws) {
 server.listen(8080, function () {
     console.log((new Date()) + ' Server is listening on port 8080');
 });
+// import express from 'express';
+// import { WebSocketServer, WebSocket } from 'ws';
+// const app = express();
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+// let httpServer = app.listen(4000, () => {
+//   console.log('Server is listening on port 4000');
+// });
+// const wsServer = new WebSocketServer({ server: httpServer });
+// wsServer.on('connection', function connection(ws) {
+//   ws.on('message', function message(data, isBinary) {
+//     console.log('received: %s', data);
+//   });
+// });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,49 @@
-// import WebSocket, { WebSocketServer } from 'ws';
-// import http from 'http';
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
 
-// const server = http.createServer(function (request: any, response: any) {
-//     console.log((new Date()) + ' Received request for ' + request.url);
-//     response.end('Hello World');
-// });
-
-// const wss = new WebSocketServer({ server });
-// let ucout = 0;
-// wss.on('error', console.error);
-
-// wss.on('connection', function connection(ws) {
-//     ws.on('message', function message(data, isBinary) {
-//         wss.clients.forEach(function each(client) {
-//             if (client !== ws && client.readyState === WebSocket.OPEN) {
-//                 client.send(data, { binary: isBinary });
-//             }
-//         });
-//     });
-
-//     console.log("user connected", ++ucout);
-//     ws.send('Hello! You are connected to the server');
-// });
-
-// server.listen(8080, function () {
-//     console.log((new Date()) + ' Server is listening on port 8080');
-// });
-
-import express from 'express';
-import { WebSocketServer, WebSocket } from 'ws';
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const server = http.createServer(function (request: any, response: any) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.end('Hello World');
 });
 
-let httpServer = app.listen(4000, () => {
-  console.log('Server is listening on port 4000');
+const wss = new WebSocketServer({ server });
+let ucout = 0;
+wss.on('error', console.error);
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function message(data, isBinary) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data, { binary: isBinary });
+            }
+        });
+    });
+
+    console.log("user connected", ++ucout);
+    ws.send('Hello! You are connected to the server');
 });
 
-const wsServer = new WebSocketServer({ server: httpServer });
-
-wsServer.on('connection', function connection(ws) {
-  ws.on('message', function message(data, isBinary) {
-    console.log('received: %s', data);
-  });
+server.listen(8080, function () {
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
+
+// import express from 'express';
+// import { WebSocketServer, WebSocket } from 'ws';
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// let httpServer = app.listen(4000, () => {
+//   console.log('Server is listening on port 4000');
+// });
+
+// const wsServer = new WebSocketServer({ server: httpServer });
+
+// wsServer.on('connection', function connection(ws) {
+//   ws.on('message', function message(data, isBinary) {
+//     console.log('received: %s', data);
+//   });
+// });
